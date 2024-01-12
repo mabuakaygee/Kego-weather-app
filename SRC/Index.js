dc011dc23375cbca736f49d9aeda241bb571a09d
mabuakaygee <153170025+mabuakaygee@users.noreply.github.com>
2024-01-12 23:18:36 +0200

    Update Index.js

--- SRC/Index.js
+++ SRC/Index.js
@@ -2,7 +2,7 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.values;
+  cityElement.innerHTML = searchInput.value;
 }
 
 let searchFormElement = document.querySelector("#search-form");
