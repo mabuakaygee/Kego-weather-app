36cad67baa2045b282ed366544c45939a66b3f7f
mabuakaygee <153170025+mabuakaygee@users.noreply.github.com>
2024-01-14 23:07:02 +0200

    Update Index.js

--- SRC/Index.js
+++ SRC/Index.js
@@ -59,7 +59,7 @@ function displayforecast() {
       forecastHtml +
       `
     <div class="weather-forecast-day">
-      <div class="weather-forecast-date">${day}</div>
+      <div class="weather-forecast-day">${day}</div>
       <div class="weather-forecast-icon">⛅</div>
       <div class="weather-forecast-temperature">
         <span class="weather-forecast-temperature-max">18°C</span>
